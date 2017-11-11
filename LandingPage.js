407aa70e554ff6f79703ecf683ef26ce6a2c5587
lb1994 <32465818+lb1994@users.noreply.github.com>
2017-11-11 18:25:37 +0000

    Trying to add profiles to Contact us Page

--- LandingPage.js
+++ LandingPage.js
@@ -1,9 +1,3 @@
 //This is the JS page for our Landing Page page
-$(document).ready(function(){
-    $(".landingpagecentralimage").hover(
-        function() {$(this).attr("src",document.underground.src='http://img.pngpicture.com/content/files/clipart/6/coffee/36183-1.png');},
-        function() {$(this).attr("src",'document.underground.src='https://imgur.com/Q8Sw295.png');
-    });
-});
 
 
